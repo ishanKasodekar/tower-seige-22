db222a9b4d7f74e230ef8c7bf6a9a316de718731
ishanKasodekar <76422610+ishanKasodekar@users.noreply.github.com>
2021-08-11 13:47:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -126,6 +126,6 @@ function mouseReleased(){
 function keyPressed(){
 //write code for extra chance of the player
 if(keyCode===32){
-Slingshot.attatch(this.polygon);
+slingShot.attach(this.polygon);
 }
 }
